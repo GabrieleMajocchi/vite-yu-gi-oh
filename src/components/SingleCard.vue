2d25b8b31c8545b6c07913b3dbc84da1247d81d6
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-19 17:18:58 +0200

    created a single card component

---
+++ src/components/SingleCard.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+        name: 'SingleCard',
+        props: {
+            cardName: String,
+            cardImg: String,
+            cardArchetype: String,
+        },
+    }
+</script>
+
+<template>
+    <div class="card rounded-0" style="width: 18rem;">
+        <img :src="cardImg" class="card-img-top rounded-0" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ cardName }}</h5>
+            <p class="card-text">{{ cardArchetype }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/mixing.scss' as *;
+    @use '../styles/partials/variables.scss' as *;
+
+    .card{
+        text-align: center;
+        background-color: #d48f38;
+        border: none;
+        div h5{
+            color: white;
+        }
+        div p{
+            font-size: 1.2rem;
+        }
+    }
+</style>
