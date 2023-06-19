2c813c423b5e4b651797d0ca8dda5e19ba1f8609
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-19 14:27:34 +0200

    Created scaffolding with vite, vue, sass, bootstrap, popper and fontawesome

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  
+  export default {
+    name: 'AppVue',
+    components:{
+        AppHeader,
+        AppMain,
+      }
+  }
+</script>
+
+<template>
+  <div>
+    <AppHeader/>
+    <AppMain/>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "bootstrap/scss/bootstrap";
+  @import './styles/general.scss';
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        name: 'AppHeader',
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
+
+<template>
+    <header>
+        <h1>App Header</h1>
+    </header>
+</template>
+
+<style lang="scss">
+    @use '../styles/partials/mixing.scss' as *;
+    @use '../styles/partials/variables.scss' as *;
+
+</style>
