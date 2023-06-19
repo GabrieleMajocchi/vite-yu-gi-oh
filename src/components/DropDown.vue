b9156b39319aca3b69d490cdc70f406b05a94d7f
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-19 17:21:24 +0200

    created dropdown for archtype selection

---
+++ src/components/DropDown.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+        name: 'DropDown',
+    }
+</script>
+
+<template>
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                Alien
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/mixing.scss' as *;
+    @use '../styles/partials/variables.scss' as *;
+
+    button{
+        color: black;
+        background-color: white;
+        border-color: black;
+    }
+
+    button:hover, button:focus, button:focus-visible, button:active{
+        color: black;
+        background-color: white;
+        border-color: black;
+    }
+</style>
