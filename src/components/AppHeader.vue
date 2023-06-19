0cf019800b0b5578665f02ce076fab9e55d364db
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-19 14:46:29 +0200

    added header navbar with title and logo img

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,12 +14,24 @@
 
 <template>
     <header>
-        <h1>App Header</h1>
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-bold" href="#">
+                <img src="../assets/img/logo.png" alt="Logo" class="d-inline-block align-text-top">
+                Yu-Gi-Oh Api
+                </a>
+            </div>
+        </nav>
     </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
     @use '../styles/partials/mixing.scss' as *;
     @use '../styles/partials/variables.scss' as *;
 
+    img{
+        width: 100px;
+        object-fit: cover;
+        height: 30px;
+    }
 </style>
